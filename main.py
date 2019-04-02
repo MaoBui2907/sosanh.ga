d060f543f68335a5a946e8d938ac2d6e9a9bbd2f
nguyenmao2101 <maonguyen199873@gmail.com>
2019-04-02 16:34:49 +0700

    Update folder structure and resources

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
+# Cài đặt với đường dẫn /
 @app.route("/")
-def hello():
-    return "Hello World from Flask"
+def homepage():
+    return render_template('homepage.html', title="So sánh giá")
+
+# Cài đặt với đường dẫn /search/?keyword='từ khóa'
+@app.route("/search/?")
+def search():
+    return render_template('search.html', title='Tìm kiếm')
+
+# Cài đặt với đường dẫn /ten-san-pham
+@app.route("/?ten-san-pham")
+def product():
+    return render_template('product.html', title='Sản phẩm')
 
 if __name__ == "__main__":
     # Only for debugging while developing
