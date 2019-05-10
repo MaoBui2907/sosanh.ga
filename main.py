f748f18987fcd2b96412c3e69e933d3d37a13f0d
nguyen mao <maonguyen199873@gmail.com>
2019-05-11 00:57:43 +0700

    Update crawl search keyword and change product page

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html', title="SoSanh.ga")
 
-# Cài đặt với đường dẫn /search/?keyword='từ khóa'
-@app.route("/compare")
-def search():
-    return render_template('search.html', title='So sánh giá')
-
-# Cài đặt với đường dẫn /ten-san-pham
+# Cài đặt với đường dẫn /timkiem?keyword=""
 @app.route("/timkiem")
 def search():
     keyword = request.args.get('keyword', default='', type=str)
@@ -26,13 +21,16 @@ def search():
     tgdd = search_thegioididong(keyword)
     vienthonga = search_vienthonga(keyword)
 
-    data = merge_data(fptshop, tgdd, vienthonga)
-    return render_template('listproduct.html', title='Danh sách sản phẩm')
+    data = merge_data([fptshop, tgdd, vienthonga])
+    return render_template('dssp.html', title='Danh sách sản phẩm', data=data[:20])
+
 # Cài đặt với đường dẫn /ten-san-pham
 @app.route("/sanpham")
 def product():
     return render_template('product.html', title='Sản phẩm')
+
+
 if __name__ == "__main__":
     # Only for debugging while developing
-    #app.run(host='0.0.0.0', debug=True, port=80)
-	app.run()
+    app.run(host='0.0.0.0', debug=True, port=5000)
+    # app.run()
